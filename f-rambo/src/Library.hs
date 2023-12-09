4c848659a20f135cbbb13b85cef8aa8ed2b7d7a6
Fernando Coiman <fcoiman@frba.utn.edu.ar>
2023-12-09 10:00:37 -0300

    agrego inmo

--- f-rambo/src/Library.hs
+++ f-rambo/src/Library.hs
@@ -1,4 +1,31 @@
-doble :: Number -> Number
-doble numero = numero + numero
-doble :: Number -> Number
-doble numero = numero + numero
+module Library where
+import PdePreludat
+
+
+doble::Number -> Number 
+doble numero = numero * 2
+data Arma = Arma {
+    tamanioCargador :: Number,
+    balas :: Number
+    } deriving (Show, Eq)
+
+
+data Rambo = Rambo {
+    armaPrincipal :: Arma,
+    armaSecundaria :: Arma
+    } deriving (Show, Eq)
+
+
+balasTotales :: Rambo -> Number
+balasTotales rambo = balas (armaPrincipal rambo) + balas (armaSecundaria rambo)
+
+disparar :: Arma -> Arma
+disparar arma 
+              | balas arma == tamanioCargador arma = arma {balas = balas arma - 2}
+              | balas arma > 1 =arma {balas = balas arma -1}
+              |otherwise = arma
+
+dispararTodo:: Rambo -> Rambo
+dispararTodo rambo = rambo{
+    armaPrincipal = disparar(armaPrincipal rambo),
+    armaSecundaria = disparar(armaSecundaria rambo)}
